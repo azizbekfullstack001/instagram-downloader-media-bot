f014a880cc58ed5eaddb04e15b39f2491e9d2331
azizbekfullstack001 <rajabovazizbek627@gmail.com>
2024-09-08 21:15:43 +0500

    first commit

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import  logging
 from aiogram import Bot, Dispatcher, executor, types
-API_TOKEN = '7526459851:AAH0FPp70mwGncEANHxgAUJpEG5nx4cZDSE'
-from api import  getUrl
-# Configure logging
-logging.basicConfig(level=logging.INFO)
+from aiogram.types import ContentType
 
+API_TOKEN = '7526459851:AAH0FPp70mwGncEANHxgAUJpEG5nx4cZDSE'
+# Configure logging
+
+logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
-@dp.message_handler()
-async  def getInstagramVideo(msg:types.Message):
-    try:
-        await msg.answer(f"🔍")
-        await msg.reply_video(getUrl(msg.text))
-    except:
-        await msg.answer("iltimos video havolasini yuboring")
+sukinish =["eshak",'mol','axmoq']
+
+@dp.message_handler(text=sukinish)
+async  def Sukinish(msg:types.Message):
+    await  msg.answer(f"so'kinma {msg.from_user.username}")
+
+@dp.message_handler(content_types=ContentType.STICKER)
+async  def info(msg:types.Message):
+    await msg.delete()
+    await  msg.answer("iltimos stiker yubormang!!!")
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
